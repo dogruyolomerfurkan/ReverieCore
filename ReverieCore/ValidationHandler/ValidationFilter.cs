4f1205daf7262f58ea64cdb9a29b89bcae7c842d
Ömer Furkan Doğruyol <ko.omerfurkan@gmail.com>
2023-02-19 19:27:45 +0300

    validation handler

---
+++ ReverieCore/ValidationHandler/ValidationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ReverieCore.ExceptionHandler.Validation;
+
+namespace ReverieCore.ValidationHandler;
+
+public class ValidationFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var errors = context.ModelState.Where(p => p.Value.Errors.Count > 0)
+                .Select(p => new ValidationError
+                {
+                    PropertyName = p.Key,
+                    ErrorMessage = p.Value.Errors.Select(e => e.ErrorMessage).First()
+                }).ToList();
+
+            throw new ValidationException(errors);
+        }
+        await next();
+    }
+}
